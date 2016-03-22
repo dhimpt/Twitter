3dee92c977d07d9c78eab45733c10998cf9a1983
dhimpt <olk42385@euaqa.com>
2016-03-22 20:39:29 +0100

    Napisana JavaDoc dokumentacija za sve metode klase TwitterPoruka

--- src/com/twitter/poruke/TwitterPoruka.java
+++ src/com/twitter/poruke/TwitterPoruka.java
@@ -1,29 +1,59 @@
 package com.twitter.poruke;
 
 public class TwitterPoruka {
+	/**
+	 * Atribut koji pamti podatke o nazivu korisnika (korisnicko ime)
+	 */
 	private String korisnik;
+	/**
+	 * Atribut koji pamti sadrzaj poruke koju korisnik salje
+	 */
 	private String poruka;
 
+	/**
+	 * Metoda koja vraca naziv korisnika
+	 * 
+	 * @return
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 
+	/**
+	 * Metoda koja upisuje naziv korisnika koji je prosledjen kao parametar
+	 * (korisnicko ime)
+	 * 
+	 * @param korisnik
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || korisnik == "")
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
 
+	/**
+	 * Metoda koja vraca String "poruka"
+	 * 
+	 * @return "poruka"
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
 
+	/**
+	 * Metoda koja upisuje sadrzaj poruke koji je prosledjen kao parametar
+	 * 
+	 * @param poruka
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka == null || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
 
+	/**
+	 * Metoda toString koja konvertuje sadrzaj objekta u String
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
